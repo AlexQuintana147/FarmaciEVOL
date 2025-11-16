491b16ace208000411945b24943511f254404c27
DiegoRuiz0109 <ruizrechartediego@gmail.com>
2025-11-15 23:00:23 -0500

    Mejoramiento Paginacion

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,7 +14,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $blogs = Blog::with('trabajador')->latest()->get();
+        $blogs = Blog::with('trabajador')->latest()->paginate(10);
         return view('dashboard.blogs.index', compact('blogs'));
     }
 
